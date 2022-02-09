68ee0c840d98f264b40a5b6d85f8a332d58c150b
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-02-08 23:03:55 -0700

    saving at end of day

---
+++ Develop/index.js
@@ -0,0 +1,64 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'what is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project. (1 to 2 sentences)',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license did you use for your project?',
+        name: 'license',
+        choices: ['Apache 2.0', 'BOOST', 'Eclipse', 'GNU', 'IBM', 'ISCLicense', 'MIT', 'Perl', 'SIL', 'Unlicense'],
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'command',
+    },
+    {
+        type: 'input',
+        message: 'What command should be used to run tests?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'n2k',
+    },
+    {
+        type: 'input',
+        message: 'What contributions were made?',
+        name: 'contribution',
+    },
+];
+
+inquirer.prompt(questions)
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
