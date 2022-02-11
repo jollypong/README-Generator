84f6ebcf881e3c75fb4d670215adfaa11a65ce85
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-02-10 20:15:23 -0700

    fixed README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,43 +1,59 @@
 //function for README Format
 function generateMarkdown(response) {
   return `# ${response.title}
-  ## License
-  <img src=https://img.shields.io/badge/License-${response.license}-blue.svg>
+## License
+<img src=https://img.shields.io/badge/License-${response.license}-blue.svg>
 
-  ## Table of Contents 
-    *[Description](#description)
-    *[Installation](#installation)
-    *[Usage](#usage)
-    *[Test](#test)
-    *[Contributor](#contributor)
+## Table of Contents 
+[Description](#description)
 
-  ## Description 
-    ${response.description}
+[Installation](#installation)
 
-    Page is currently deployed at: 
-    ['Deployed Link'](https://${response.username}.github.io/${response.title}/) 
+[Usage](#usage)
+
+[Test](#test)
+
+[Screenshot](#screenshot)
+
+[Contributors](#contributors)
+
+## Description 
+${response.description}
+
+Page is currently deployed at: 
+
+['Deployed Page'](https://${response.username}.github.io/${response.title}/) 
     
-    You can also check the repository at: 
-    ['GitHub Repo'](https://github.com/${response.username}/${response.title})
+You can also check the repository at: 
 
-  ## Installation
+['GitHub Repo'](https://github.com/${response.username}/${response.title})
+
+## Installation
+Following code is required for installation: 
+
     ${response.installation}
 
-  ## Usage
+## Usage
+  In order to use this application, please run following command: 
+
     ${response.runCommand}
 
-  ## Test
+## Test
+  In order for testing please follow below code
+
     ${response.test}
 
-  ## Screenshot
+## Screenshot
 
-  ## Contributors
-  Got any questions or want to contribute to this project? 
-  
-  ${response.contributor} 
+## Contributors
 
-    *['GitHub'] (https//:github.com/${response.username}) 
-    *Email: ${response.email}`
-};
+${response.contributor} 
+
+Got any questions or want to contribute to this project? 
+Contact me via: 
+
+*[${response.username}](https//:github.com/${response.username}) 
+
+*Email: ${response.email}`};
 
 module.exports = generateMarkdown;
--- README.md
+++ README.md
@@ -1,38 +1,53 @@
-# README-generator
-  ## License
-  <img src=https://img.shields.io/badge/License-No License Used-blue.svg>
+# asdfawdf
+## License
+<img src=https://img.shields.io/badge/License-No License Used-blue.svg>
 
-  ## Table of Contents 
-    *[Description](#description)
-    *[Installation](#installation)
-    *[Usage](#usage)
-    *[Test](#test)
-    *[Contributor](#contributor)
+## Table of Contents 
+[Description](#description)
 
-  ## Description 
-    This application was created using NodeJS and Javascript.It generates a README.md file quickly and easily using command-line inputs from the user.
+[Installation](#installation)
 
-    Page is currently deployed at: 
-    ['Deployed Link'](https://Jollypong.github.io/README-generator/) 
+[Usage](#usage)
+
+[Test](#test)
+
+[Contributors](#contributors)
+
+## Description 
+asdfasdfa
+
+Page is currently deployed at: 
+
+['Deployed Page'](https://asdfasdf.github.io/asdfawdf/) 
     
-    You can also check the repository at: 
-    ['GitHub Repo'](https://github.com/Jollypong/README-generator)
+You can also check the repository at: 
 
-  ## Installation
-    npm install -y
+['GitHub Repo'](https://github.com/asdfasdf/asdfawdf)
 
-  ## Usage
-    node index.js
+## Installation
+Following code is required for installation: 
 
-  ## Test
-    n/a
+    asdfasdfasd
 
-  ## Screenshot
+## Usage
+  In order to use this application, please run following command: 
 
-  ## Contributors
-  Got any questions or want to contribute to this project? 
-  
-  Joe Lee 
+    sadfasdfasdf
 
-    *['GitHub'] (https//:github.com/Jollypong) 
-    *Email: joeleegc@gmail.com
+## Test
+  In order for testing please follow below code
+
+    asdfasdfasdf
+
+## Screenshot
+
+## Contributors
+
+asdfasdfasdfa 
+
+Got any questions or want to contribute to this project? 
+Contact me via: 
+
+*[asdfasdf](https//:github.com/asdfasdf) 
+
+*Email: asdfasdf
